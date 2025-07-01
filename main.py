d2681e4ef5ce5f42756f9a9c43ba70b66670feaf
Scotasloth <rossmcc76@gmail.com>
2025-07-01 22:17:44 +0100

    Created INI

--- main.py
+++ main.py
@@ -1,13 +1,51 @@
+import sys
+import os
+import customtkinter as ctk
+import configparser
+
 def main():
-   return
+   
+   existsIni = checkIni()
+   if existsIni == True:
+      return
+   
+   else:
+      createIni()
+   
+   root = ctk.CTk()
+   root.geometry("500x600")
+   root.title("Finance Tracker")
+   #root.set_appearance_mode("Dark")
+
+   root.mainloop()
 
 def createIni():
-    return
+    
+    config = configparser.ConfigParser()
+
+    config["Settings"] = {
+       "total": "0",
+       "pay": "12.26",
+       "tax": "20",
+       "rent": "575",
+       "council tax": "164",
+       "wifi": "27",
+       "power": "57"
+    }
+
+    with open('config.ini', 'w') as configfile:
+        config.write(configfile)
+
+    print("INI file created with default settings.")
+
+def checkIni():
+   
+   return os.path.exists('config.ini')
 
 def readIni():
    return
 
-def updateTotal():
+def updateTotal(newTotal):
    return
 
 def calcPay():
