8d68f4f8e3f476c3acc084fb1af58eb81a639bc1
Scotasloth <rossmcc76@gmail.com>
2025-07-01 21:44:24 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+def main():
+   return
+
+def createIni():
+    return
+
+def readIni():
+   return
+
+def updateTotal():
+   return
+
+def calcPay():
+   return
+
+def schedule():
+   return
+
+if __name__ == "__main__":
+   main().run()
